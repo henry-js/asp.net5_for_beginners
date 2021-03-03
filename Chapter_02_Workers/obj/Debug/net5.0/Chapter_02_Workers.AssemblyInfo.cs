ed8648e97b7dff455a895b2251ef5e9611802622
otaconjh <41576511+otaconjh@users.noreply.github.com>
2021-03-03 15:18:37 +0000

    restart

--- Chapter_02_Workers/obj/Debug/net5.0/Chapter_02_Workers.AssemblyInfo.cs
+++ Chapter_02_Workers/obj/Debug/net5.0/Chapter_02_Workers.AssemblyInfo.cs
@@ -11,7 +11,6 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("dotnet-Chapter_02_Workers-1AF0B36C-A07F-4852-BBBE-3ACBC251DEE3")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chapter_02_Workers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
