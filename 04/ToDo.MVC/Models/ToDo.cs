c24a571f9e779652570a8d455deb13a1b33d7a20
henry-js <henry-js@outlook.com>
2021-04-12 01:01:52 +0100

    re-up

---
+++ 04/ToDo.MVC/Models/ToDo.cs
@@ -0,0 +1,9 @@
+namespace ToDo.MVC.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ 04/ToDo.MVC/Models/TodoDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDo.MVC.Models
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options) { }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
