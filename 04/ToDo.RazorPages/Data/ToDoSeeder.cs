2f73e50330b1ccb70291526c3265628b647cdf0a
James <otaconjh@outlook.com>
2021-04-16 13:24:37 +0100

    started Todo.RazorPages

---
+++ 04/ToDo.RazorPages/Data/ToDoSeeder.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ToDo.RazorPages.Data
+{
+    public class TodoDbSeeder
+    {
+        public static void Seed(IServiceProvider serviceProvider)
+        {
+            var context = new TodoDbContext(serviceProvider.GetRequiredService<DbContextOptions<TodoDbContext>>());
+
+            // Look for any todos.
+            if (context.Todos.Any())
+            {
+                // if we get here then the data is already seeded.
+                return;
+            }
+
+            context.Todos.AddRange(
+                new Todo
+                {
+                    Id = 1,
+                    TaskName = "Work on book chapter",
+                    IsComplete = false
+                },
+                new Todo
+                {
+                    Id = 2,
+                    TaskName = "Create video content",
+                    IsComplete = false
+                },
+                new Todo
+                {
+                    Id = 3,
+                    TaskName = "Go for a run",
+                    IsComplete = false
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
