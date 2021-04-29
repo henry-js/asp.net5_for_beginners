da136d4103b228cbc3675ba9af96d68b2aef2480
James <79054685+henry-js@users.noreply.github.com>
2021-04-29 13:48:52 +0100

    progress

--- 05/PlaceApi/Db/PlaceDbContext.cs
+++ 05/PlaceApi/Db/PlaceDbContext.cs
@@ -3,9 +3,9 @@ using PlaceApi.Db.Models;
 
 namespace PlaceApi.Db
 {
-    public class PlaceDbCOntext: DbContext
+    public class PlaceDbContext: DbContext
     {
-        public PlaceDbCOntext(DbContextOptions<PlaceDbCOntext> options) : base(options)
+        public PlaceDbContext(DbContextOptions<PlaceDbContext> options) : base(options)
         {}
         public DbSet<Place> Places { get; set; }
     }
